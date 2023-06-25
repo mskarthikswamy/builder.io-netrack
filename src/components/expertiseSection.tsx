00c274fd0d47e7006d0b624e092f93531db5e4f3
mskarthikswamy <karthik.swamy@makoitlab.com>
2023-06-25 12:04:00 +0530

    expertise and mission section

--- src/components/expertiseSection.tsx
+++ src/components/expertiseSection.tsx
@@ -7,14 +7,28 @@ interface props {
 }
 const ExpertiseSection = (props: props) => {
     return (<>
-        <h2 style={{ textAlign: "center" }}>{props?.reviews[0]?.heading}</h2>
-        <p style={{ textAlign: "center", width: "80%" }}>{props?.reviews[0]?.content}</p>
-        <div style={{ display: "flex" }}>
-            {props?.reviews?.map((review) => {
-                return <><p>{review?.info}</p>
-                </>
-            })}
+
+        <h2 style={{ textAlign: "center", maxWidth: "900px", margin: "0 auto" }}>{props?.reviews[0]?.heading}</h2>
+        <div>
+            <p style={{ textAlign: "center", maxWidth: "900px", margin: "0 auto", paddingTop: "12px", color: " #6d6d6d" }}>{props?.reviews[0]?.content}</p>
         </div>
+
+        <div style={{ display: "flex", height: "200px", gap: "5px", margin: "20px 20px 20px 20px" }}>
+            {props?.reviews?.map((review) => {
+
+                return <>
+                    <div style={{ padding: "2px 16px", width: "40%", background: "#22316a" }}>
+                        <p style={{ color: "white", textAlign: "center", marginTop: "60px" }}>{review?.info}</p>
+                    </div>
+
+
+
+
+                </>
+
+
+            })}
+        </div >
     </>)
 }
 
--- src/components/missionSection.tsx
+++ src/components/missionSection.tsx
@@ -17,9 +17,9 @@ const MissionSection = (props: props) => {
                 <div>
                     <img src={props?.reviews[0]?.image} style={{ width: "272px", height: "222px" }}></img>
                 </div>
-                <div>
+                <div style={{ paddingTop: "20px" }}>
                     <h4 style={{ fontWeight: "800px", fontFamily: "sans-serif" }}>{props?.reviews[0]?.label}</h4>
-                    <p style={{ color: "#6d6d6d" }}>{props?.reviews[0]?.content}</p>
+                    <p style={{ color: "#6d6d6d", fontFamily: "sans-serif" }}>{props?.reviews[0]?.content}</p>
                 </div>
             </div>
 
@@ -32,9 +32,9 @@ const MissionSection = (props: props) => {
                 <div>
                     <img src={props?.reviews[1]?.image} style={{ width: "272px", height: "222px" }}></img>
                 </div>
-                <div>
+                <div style={{ marginTop: "30px" }}>
                     <h3 style={{ fontWeight: "800px", fontFamily: "sans-serif" }}>{props?.reviews[1]?.label}</h3>
-                    <p style={{ color: "#6d6d6d" }}>{props?.reviews[1]?.content}</p>
+                    <p style={{ color: "#6d6d6d", fontFamily: "sans-serif" }}>{props?.reviews[1]?.content}</p>
                 </div>
             </div>
 
@@ -43,12 +43,12 @@ const MissionSection = (props: props) => {
     const ThirdReview = () => {
         return (
             <div style={{ display: "flex" }}>
-                <div>
-                    <img src={props?.reviews[2]?.image} style={{ width: "272px", height: "222px" }}></img>
+                <div style={{ margin: "62px 92px 0 0" }}>
+                    <h3 style={{ fontWeight: "800px", fontFamily: "sans-serif" }}>{props?.reviews[2]?.label}</h3>
+                    <p style={{ color: "#6d6d6d", fontFamily: "sans-serif" }}>{props?.reviews[2]?.content}</p>
                 </div>
                 <div>
-                    <h3 style={{ fontWeight: "800px", fontFamily: "sans-serif" }}>{props?.reviews[2]?.label}</h3>
-                    <p style={{ color: "#6d6d6d" }}>{props?.reviews[2]?.content}</p>
+                    <img src={props?.reviews[2]?.image} style={{ width: "390px", height: "360px" }}></img>
                 </div>
             </div>
 
@@ -60,12 +60,12 @@ const MissionSection = (props: props) => {
         <div>
             <div style={{ textAlign: "center" }}>
                 <h2 style={{ fontWeight: "800" }}>Our Mission</h2>
-                <div style={{ padding: "20px , 20px , 20px", fontSize: "20px", color: "#6d6d6d" }}>
+                <div style={{ padding: "60px , 20px , 60px", fontSize: "20px", color: "#6d6d6d" }}>
                     <p>Unlocking the power of technology to transform lives and businesses - NetTrackers, your partner for progress and success!!!</p>
                 </div>
 
             </div>
-            <div style={{ display: "flex" }}>
+            <div style={{ display: "flex", padding: "60px 20px 20px 20px" }}>
                 <div style={{ width: '50%', }}>
                     <FirstReview />
                 </div>
